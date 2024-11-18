e848b391cc57e76ed1ad2ae2e17c21654d22bac8
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-18 12:53:38 -0500

    Create stod2.is.config.js

---
+++ sites/stod2.is/stod2.is.config.js
@@ -0,0 +1,54 @@
+const dayjs = require('dayjs')
+
+module.exports = {
+  site: 'stod2.is',
+  days: 2,
+  request: {
+    cache: {
+      ttl: 60 * 60 * 1000 // 1 hour
+    }
+  },
+  url({ channel }) {
+    return `https://api.stod2.is/dagskra/api/${channel.site_id}`
+  },
+  parser({ content, channel }) {
+    let programs = []
+    const items = parseItems(content, channel)
+    items.forEach(item => {
+      if (!item) return
+      const start = dayjs(item.upphaf)
+      const stop = start.add(item.slott, 'm')
+      programs.push({
+        title: item.isltitill,
+        sub_title: item.undirtitill,
+        description: item.lysing,
+        start,
+        stop
+      })
+    })
+
+    return programs
+  },
+  async channels() {
+    const axios = require('axios')
+    const data = await axios
+      .get(`https://api.stod2.is/dagskra/api`)
+      .then(r => r.data)
+      .catch(console.log)
+
+    return data.channels.map(item => {
+      return {
+        lang: 'is',
+        site_id: item.id
+      }
+    })
+  }
+}
+
+function parseItems(content, channel) {
+  const data = JSON.parse(content)
+  if (!data || !Array.isArray(data.channels)) return []
+  const channelData = data.channels.find(i => i.id === channel.site_id)
+
+  return channelData && Array.isArray(channelData.events) ? channelData.events : []
+}
