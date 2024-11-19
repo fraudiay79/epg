61efa62ed8eb2c4003ebbe002c19c3b823431d21
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-19 15:02:38 -0500

    Create yes.co.il.config.js

---
+++ sites/yes.co.il/yes.co.il.config.js
@@ -0,0 +1,56 @@
+const dayjs = require('dayjs')
+
+module.exports = {
+  site: 'yes.co.il',
+  days: 2,
+  request: {
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+    }
+    cache: {
+      ttl: 60 * 60 * 1000 // 1 hour
+    }
+  },
+  url({ date }) {
+    return `https://www.yes.co.il/o/yes/servletlinearsched/getscheduale?startdate=${date.format('YYYYMMDD')}&p_auth=bHTQLx7d`
+  },
+  parser({ content, channel }) {
+    let programs = []
+    const items = parseItems(content, channel)
+    items.forEach(item => {
+      if (!item.details) return
+      const start = dayjs(item.startTime)
+      const stop = start.add(item.broadcastItemDuration, 'm')
+      programs.push({
+        title: item.scheduleItemName,
+        description: item.scheduleItemSynopsis,
+        start,
+        stop
+      })
+    })
+
+    return programs
+  },
+  async channels() {
+    const axios = require('axios')
+    const data = await axios
+      .get(`https://www.yes.co.il/o/yes/servletlinearsched/getchannels?p_auth=bHTQLx7d`)
+      .then(r => r.data)
+      .catch(console.log)
+
+    return data.channels.map(item => {
+      return {
+        lang: 'he',
+        site_id: item.channelId,
+        name: item.channelName
+      }
+    })
+  }
+}
+
+function parseItems(content, channel) {
+  const data = JSON.parse(content, channel)
+
+  return data
+}
