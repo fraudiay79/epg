c50cae0ab3f0e7d13e1e566fe3fad1b986d10cf6
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-21 12:23:58 -0500

    Update novatv.is.config.js

--- sites/novatv.is/novatv.is.config.js
+++ sites/novatv.is/novatv.is.config.js
@@ -9,7 +9,7 @@ module.exports = {
     }
   },
   url({ date }) {
-    return `https://exposure.api.redbee.live/v2/customer/Nova/businessunit/novatvprod/epg/date/${date.format('YYYY-MM-DD')}?pageSize=5000`
+    return `https://exposure.api.redbee.live/v2/customer/Nova/businessunit/novatvprod/epg/date/${date.format('YYYY-MM-DD')}`
   },
   parser({ content, channel }) {
     let programs = []
@@ -19,8 +19,8 @@ module.exports = {
       const start = dayjs(item.programs.startTime)
       const stop = dayjs(item.programs.endTime)
       programs.push({
-        title: item.programs.asset.title,
-        description: item.programs.asset.description,
+        title: item.programs.title,
+        description: item.programs.description,
         start,
         stop
       })
