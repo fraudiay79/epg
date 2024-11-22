ae2e389afcedc21ff38474d873a8cba7b7f2528b
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-22 10:21:10 -0500

    Update movistarplus.es.config.js

--- sites/movistarplus.es/movistarplus.es.config.js
+++ sites/movistarplus.es/movistarplus.es.config.js
@@ -1,27 +1,49 @@
+// credit for this fix goes to davidclaeysquinones for his PR on https://github.com/iptv-org/epg/pull/2440
+
 const { DateTime } = require('luxon')
+
+const API_CHANNEL_ENDPOINT = 'https://www.movistarplus.es/programacion-tv'
+const API_PROGRAM_ENDPOINT = 'https://comunicacion.movistarplus.es'
+const API_IMAGE_ENDPOINT = 'https://www.movistarplus.es/recorte/n/caratulaH/';
 
 module.exports = {
   site: 'movistarplus.es',
   days: 2,
-  url: function ({ date }) {
-    return `https://www.movistarplus.es/programacion-tv/${date.format('YYYY-MM-DD')}?v=json`
+  url: function ({ channel, date }) {
+    return `${API_PROGRAM_ENDPOINT}/wp-admin/admin-ajax.php`
+  },
+  request: {
+    method: 'POST',
+    headers: {
+      Origin: API_PROGRAM_ENDPOINT,
+      Referer: `${API_PROGRAM_ENDPOINT}/programacion/`,
+      "Content-Type" : 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    data: function ({ channel, date }) {
+      return {
+        action: 'getProgramation',
+        day: date.format('YYYY-MM-DD'),
+        "channels[]": channel.site_id
+      }
+    }
   },
   parser({ content, channel, date }) {
     let programs = []
     let items = parseItems(content, channel)
     if (!items.length) return programs
     let guideDate = date
+
     items.forEach(item => {
       let startTime = DateTime.fromFormat(
-        `${guideDate.format('YYYY-MM-DD')} ${item.HORA_INICIO}`,
-        'yyyy-MM-dd HH:mm',
+        `${item.f_evento_rejilla}`,
+        'yyyy-MM-dd HH:mm:ss',
         {
           zone: 'Europe/Madrid'
         }
       ).toUTC()
       let stopTime = DateTime.fromFormat(
-        `${guideDate.format('YYYY-MM-DD')} ${item.HORA_FIN}`,
-        'yyyy-MM-dd HH:mm',
+        `${item.f_fin_evento_rejilla}`,
+        'yyyy-MM-dd HH:mm:ss',
         {
           zone: 'Europe/Madrid'
         }
@@ -31,8 +53,9 @@ module.exports = {
         stopTime = stopTime.plus({ days: 1 })
       }
       programs.push({
-        title: item.TITULO,
-        category: item.GENERO,
+        title: item.des_evento_rejilla,
+        icon: parseIcon(item, channel),
+        category: item.des_genero,
         start: startTime,
         stop: stopTime
       })
@@ -43,7 +66,7 @@ module.exports = {
     const axios = require('axios')
     const dayjs = require('dayjs')
     const data = await axios
-      .get(`https://www.movistarplus.es/programacion-tv/${dayjs().format('YYYY-MM-DD')}?v=json`)
+      .get(`${API_CHANNEL_ENDPOINT}/${dayjs().format('YYYY-MM-DD')}?v=json`)
       .then(r => r.data)
       .catch(console.log)
 
@@ -57,9 +80,15 @@ module.exports = {
   }
 }
 
+function parseIcon(item, channel) {
+  return `${API_IMAGE_ENDPOINT}/M${channel.site_id}P${item.ELEMENTO}`;
+}
+
 function parseItems(content, channel) {
   const json = typeof content === 'string' ? JSON.parse(content) : content
-  if (!(`${channel.site_id}-CODE` in json.data)) return []
-  const data = json.data[`${channel.site_id}-CODE`]
-  return data ? data.PROGRAMAS : []
+  const data = json.channelsProgram;
+
+  if(data.length != 1)
+    return []
+  return data[0];
 }
