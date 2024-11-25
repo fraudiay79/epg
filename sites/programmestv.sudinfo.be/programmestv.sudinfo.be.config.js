51e2bbd7a0a330e6bff37a074c85c5ba0fb7e1ec
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-25 14:13:19 -0500

    Update programmestv.sudinfo.be.config.js

--- sites/programmestv.sudinfo.be/programmestv.sudinfo.be.config.js
+++ sites/programmestv.sudinfo.be/programmestv.sudinfo.be.config.js
@@ -23,10 +23,10 @@ module.exports = {
     const items = parseItems(content)
 
     items.forEach(item => {
-        const start = dayjs.utc(item.filteredContent.airingStartDateTime)
-        const stop = dayjs.utc(item.filteredContent.airingEndDateTime)
+        const start = dayjs.utc(item.pageProps.filteredContent.airingStartDateTime)
+        const stop = dayjs.utc(item.pageProps.filteredContent.airingEndDateTime)
       programs.push({
-          title: item.filteredContent.title,
+          title: item.pageProps.filteredContent.title,
           start,
           stop
       })
@@ -53,7 +53,7 @@ module.exports = {
 
 function parseItems(content) {
   const data = JSON.parse(content)
-  if (!data || !Array.isArray(data.filteredContent)) return []
+  if (!data || !Array.isArray(data.pageProps)) return []
 
-  return data.filteredContent
+  return data.pageProps
 }
