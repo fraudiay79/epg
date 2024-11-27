03ef2712171c9d028909c23c12ec1d66cdfda790
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-27 11:21:25 -0500

    Update cablenet.com.cy.config.js

--- sites/cablenet.com.cy/cablenet.com.cy.config.js
+++ sites/cablenet.com.cy/cablenet.com.cy.config.js
@@ -37,7 +37,7 @@ module.exports = {
       .get(`https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg2024-11-27.json`)
       .then(r => r.data)
       .catch(console.log)
-    return data.map(item => {
+    return data(item => {
       return {
         lang: 'el',
 	name: item.ch,
