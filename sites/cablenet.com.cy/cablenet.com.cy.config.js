42cb6bab1a3aabc1f4725ae5963676a454f52901
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-27 11:52:51 -0500

    Update cablenet.com.cy.config.js

--- sites/cablenet.com.cy/cablenet.com.cy.config.js
+++ sites/cablenet.com.cy/cablenet.com.cy.config.js
@@ -37,7 +37,7 @@ module.exports = {
       .get(`https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg2024-11-27.json`)
       .then(r => r.data)
       .catch(console.log)
-    const channels = parseItems(channel)
+    const channels = parseItems(content)
     return Object.values(channels).map(item => {
       return {
         lang: 'el',
