90d90eb346529e78f1943ae44362a7132da2e1c2
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-27 11:09:47 -0500

    Update cablenet.com.cy.config.js

--- sites/cablenet.com.cy/cablenet.com.cy.config.js
+++ sites/cablenet.com.cy/cablenet.com.cy.config.js
@@ -31,7 +31,7 @@ module.exports = {
 
     return programs
   },
-  async channels(date) {
+  async channels({ date }) {
     const axios = require('axios')
     const data = await axios
       .get(`https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg${date.format('YYYY-MM-DD')}.json`)
