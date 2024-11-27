e294996d24f3f6e41af7543aa6e0c7d5c3b5b835
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-27 11:05:42 -0500

    Create cablenet.com.cy.config.js

---
+++ sites/cablenet.com.cy/cablenet.com.cy.config.js
@@ -0,0 +1,54 @@
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+
+dayjs.extend(utc)
+
+module.exports = {
+  site: 'cablenet.com.cy',
+  days: 6,
+  request: {
+    cache: {
+      ttl: 60 * 60 * 1000 // 1 hour
+    }
+  },
+  url({ date }) {
+    return `https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg${date.format('YYYY-MM-DD')}.json`
+  },
+  parser({ content, date }) {
+    let programs = []
+    const items = parseItems(content)
+    items.forEach(item => {
+      if (!item) return
+      const start = dayjs.utc(item.pr.df)
+      const stop = dayjs.utc(item.pr.dt)
+      programs.push({
+        title: item.pr.ti,
+        description: item.pr.sd,
+        start,
+        stop
+      })
+    })
+
+    return programs
+  },
+  async channels(date) {
+    const axios = require('axios')
+    const data = await axios
+      .get(`https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg${date.format('YYYY-MM-DD')}.json`)
+      .then(r => r.data)
+      .catch(console.log)
+    return data.channels.map(item => {
+      return {
+        lang: 'el',
+		name: item.ch,
+        site_id: item.id
+      }
+    })
+  }
+}
+  
+function parseItems(content) {
+  const data = JSON.parse(content)
+
+  return data.pr
+}
