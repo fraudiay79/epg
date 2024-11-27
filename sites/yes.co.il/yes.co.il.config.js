586022a3170ce2336f49cd45df8bf97d3c52ab9b
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-27 14:25:06 -0500

    Update yes.co.il.config.js

--- sites/yes.co.il/yes.co.il.config.js
+++ sites/yes.co.il/yes.co.il.config.js
@@ -12,51 +12,44 @@ module.exports = {
   url({ date }) {
     return `https://www.yes.co.il/o/yes/servletlinearsched/getscheduale?startdate=${date.format('YYYYMMDD')}&p_auth=b1IRvxWi`
   },
-  parser({ content, date }) {
-    let programs = []
-    const items = parseItems(content)
-    items.forEach(item => {
-      if (!item.details) return
-      const start = item.startTime
-      const stop = start.add(item.broadcastItemDuration, 'm')
-      programs.push({
-        title: item.scheduleItemName,
-        description: item.scheduleItemSynopsis,
-        start,
-        stop
-      })
+  function parser({ content, date }) {
+  const programs = [];
+  const items = parseItems(content);
+
+  items.forEach(item => {
+    if (!item.details) return;
+
+    const start = DateTime.fromISO(item.startDate + 'T' + item.startTime, { zone: 'Africa/Jerusalem' });
+    const stop = start.plus({ minutes: item.broadcastItemDuration * 60 });
+
+    programs.push({
+      title: item.scheduleItemName,
+      description: item.scheduleItemSynopsis,
+      start,
+      stop
+    });
+  });
+
+  return programs;
+},
+  async channels() {
+    const axios = require('axios')
+    const data = await axios
+      .get(`https://www.yes.co.il/o/yes/servletlinearsched/getscheduale?startdate=20241127&p_auth=mmKfNOfX`)
+      .then(r => r.data)
+      .catch(console.log)
+    return data.channels.map(item => {
+      return {
+        lang: 'he',
+        name: item.channelName,
+        site_id: item.channelId
+      }
     })
-
-    return programs
-  },
-  async function getChannels() {
-  try {
-    const response = await axios.get(`https://www.yes.co.il/o/yes/servletlinearsched/getchannels?p_auth=b1IRvxWi`);
-    const data = response.data;
-
-    // Check if data.channels exists before processing
-    if (!data.channels) {
-      throw new Error("Missing channels in response data");
-    }
-
-    // Extract channel data
-    const channelData = data.channels.map(item => ({
-      lang: 'he',
-      site_id: item.channelId,
-      name: item.channelName
-    }));
-
-    return channelData;
-  } catch (error) {
-    console.error("Error fetching channel data:", error);
-    // You can also throw the error to be handled elsewhere
-    // throw error;
   }
 }
-}
 
-function parseItems(content, channel) {
-  const data = JSON.parse(content, channel)
+function parseItems(content) {
+  const data = JSON.parse(content)
 
   return data
 }
