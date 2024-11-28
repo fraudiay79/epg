144ecde7bdcd909e787933e7f8db8ed156d4f89c
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-28 13:02:16 -0500

    Update iptvx.one.config.js

--- sites/iptvx.one/iptvx.one.config.js
+++ sites/iptvx.one/iptvx.one.config.js
@@ -14,7 +14,7 @@ module.exports = {
   url({ channel }) {
     return `https://epg.iptvx.one/api/id/${channel.site_id}.json`
   },
-  parser({ content }) {
+  parser({ content, channel }) {
     // Assuming parseItems is specific to iptvx.one format
     const items = parseItems(content)
     let programs = []
