c4a1b6855e2fa837cbc423ec7cf2b43e3a6e8cdf
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-02 11:29:57 -0500

    Update novatv.is.config.js

--- sites/novatv.is/novatv.is.config.js
+++ sites/novatv.is/novatv.is.config.js
@@ -1,11 +1,11 @@
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
-const customParseFormat = require('dayjs/plugin/customParseFormat');
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(customParseFormat);
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
 
 module.exports = {
   site: 'novatv.is',
@@ -16,41 +16,42 @@ module.exports = {
     }
   },
   url({ channel, date }) {
-    return `https://exposure.api.redbee.live/v2/customer/Nova/businessunit/novatvprod/epg/${channel.site_id}/date/${date.format('YYYY-MM-DD')}`;
+    return `https://exposure.api.redbee.live/v2/customer/Nova/businessunit/novatvprod/epg/${channel.site_id}/date/${date.format('YYYY-MM-DD')}`
   },
   parser: function ({ content }) {
-    const programs = [];
+    const programs = []
 
-  const data = JSON.parse(content);
-  data.programs.forEach(program => {
+    const data = JSON.parse(content);
+    data.programs.forEach(program => {
     const localizedData = program.asset.localized.find(loc => loc.locale === 'is') || program.asset.localized[0]; // default to first if 'is' locale not found
-
+    const start = dayjs.utc(program.asset.startTime)
+    const stop = dayjs.utc(program.asset.endTime)
     const programData = {
       title: localizedData.title,
       description: localizedData.longDescription || localizedData.extendedDescription || 'No description available',
-      startTime: program.startTime,
-      endTime: program.endTime
-    };
+      start,
+      stop
+    }
 
-    programs.push(programData);
-  });
+    programs.push(programData)
+  })
 
-  return programs;
+  return programs
   },
   async channels() {
-    const axios = require('axios');
+    const axios = require('axios')
     try {
-      const response = await axios.get(`https://exposure.api.redbee.live/v1/customer/Nova/businessunit/novatvprod/content/asset?assetType=TV_CHANNEL`);
+      const response = await axios.get(`https://exposure.api.redbee.live/v1/customer/Nova/businessunit/novatvprod/content/asset?assetType=TV_CHANNEL`)
       return response.data.items.map(item => {
         return {
           lang: 'is',
           name: item.localized[0].title,
           site_id: item.assetId
-        };
-      });
+        }
+      })
     } catch (error) {
-      console.error('Error fetching channels:', error);
-      return [];
+      console.error('Error fetching channels:', error)
+      return []
     }
   }
 }
