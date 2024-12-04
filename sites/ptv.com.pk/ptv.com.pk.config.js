a4c0a8349a291a58f848a9dff68890cdbfdd7c10
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-04 15:29:45 -0500

    Update ptv.com.pk.config.js

--- sites/ptv.com.pk/ptv.com.pk.config.js
+++ sites/ptv.com.pk/ptv.com.pk.config.js
@@ -1,54 +1,63 @@
-const dayjs = require('dayjs')
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-const customParseFormat = require('dayjs/plugin/customParseFormat')
+const axios = require('axios');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.extend(customParseFormat)
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
 
 module.exports = {
   site: 'ptv.com.pk',
   channels: 'ptv.com.pk.channels.xml',
   days: 2,
-  url: function ({ date, channel }) {
-    const daysOfWeek = {
-      0: 'Monday',
-      1: 'Tuesday',
-      2: 'Wednesday',
-      3: 'Thursday',
-      4: 'Friday',
-      5: 'Saturday',
-      6: 'Sunday'
+  request: {
+    cache: {
+      ttl: 60 * 60 * 1000 // 1 hour
     }
-    const day = date.day()
-
-    return `https://ptv.com.pk/getShowTvGuide?channel=${channel.site_id}&nameofday=${daysOfWeek[day]}`
   },
-  parser: function ({ content, date }) {
-    const programs = []
-    const data = parseContent(content)
-    data.forEach(item => {
-	let start = parseStart(item)
-      if (prev) {
-        if (start.isBefore(prev.start)) {
-          start = start.add(1, 'd')
-          date = date.add(1, 'd')
-        }
-        prev.stop = start
-      }
-      const stop = start.add(60, 'm')
-    programs.push({
-        title: item.programName,
-        start,
-        stop
-      })
-    })
-
-    return programs
+  url({ channel, date }) {
+    const nameOfDay = date.format('dddd');
+    return `https://ptv.com.pk/getShowTvGuide?channel=${channel.site_id}&nameofday=${nameOfDay}`;
   },
+  parser({ content }) {
+    let programs = [];
 
+    try {
+      const items = JSON.parse(content);
+      items.forEach(item => {
+        const programTime = parseProgramTime(item.programTime);
+        programs.push({
+          title: item.programName,
+          programTime: item.programTime,
+          start: programTime,
+          description: item.descr || 'No description available'
+        });
+      });
+    } catch (error) {
+      console.error("Error parsing content:", error);
+    }
 
-function parseStart(item) {
-  return dayjs(item.programTime).toJSON()
+    return programs;
+  }
+};
+
+function parseProgramTime(timeStr) {
+  // Split the time string into components
+  const [time, period] = timeStr.split(' ');
+  let [hours, minutes] = time.split('.').map(Number);
+
+  // Convert 12-hour time to 24-hour time
+  if (period.toLowerCase() === 'pm' && hours !== 12) {
+    hours += 12;
+  }
+  if (period.toLowerCase() === 'am' && hours === 12) {
+    hours = 0;
+  }
+
+  // Format the time in HH:mm:ss format
+  const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:00`;
+
+  return formattedTime;
 }
