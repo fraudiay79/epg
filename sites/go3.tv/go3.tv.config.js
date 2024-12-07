5850cb59e177954393d5792a883da8448a94730a
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-06 23:00:38 -0500

    Create go3.tv.config.js

---
+++ sites/go3.tv/go3.tv.config.js
@@ -0,0 +1,59 @@
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+const axios = require('axios');
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
+
+module.exports = {
+  site: 'go3.tv',
+  days: 2,
+  request: {
+    cache: {
+      ttl: 60 * 60 * 1000 // 1 hour
+    }
+  },
+  url({ channel, date }) {
+    const formattedDate = date.format('YYYY-MM-DD');
+    const since = `${formattedDate}T00:00-0500`;
+    const till = `${formattedDate}T23:59-0500`;
+    return `https://go3.tv/api/products/lives/programmes?liveId[]=${channel.site_id}&since=${since}&till=${till}&platform=BROWSER&lang=EN&tenant=OM_EE`;
+  },
+  parser: function ({ content }) {
+    const programs = [];
+    const data = JSON.parse(content);
+
+    Object.keys(data.channels).forEach(channelId => {
+      const channelPrograms = data.channels[channelId];
+      channelPrograms.forEach(program => {
+        const start = dayjs.utc(program.start_time).format('YYYY-MM-DDTHH:mm:ssZ');
+        const stop = dayjs.utc(program.end_time).format('YYYY-MM-DDTHH:mm:ssZ');
+        const programData = {
+          title: program.description,
+          description: program.full_description || program.description || 'No description available',
+          start,
+          stop
+        };
+        programs.push(programData);
+      });
+    });
+
+    return programs;
+  },
+  async channels() {
+    try {
+      const response = await axios.get(`https://go3.tv/api/products/sections/v2/live_tv?platform=BROWSER&lang=EE&tenant=OM_EE`);
+      return response.data.sections[0].items.map(item => ({
+        lang: 'ee',
+        name: item.title,
+        site_id: item.id
+      }));
+    } catch (error) {
+      console.error('Error fetching channels:', error);
+      return [];
+    }
+  }
+};
