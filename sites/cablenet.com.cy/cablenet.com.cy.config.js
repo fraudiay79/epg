5fed646516c936d9eed01f84aa485de07d339f35
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-08 11:59:08 -0500

    Update cablenet.com.cy.config.js

--- sites/cablenet.com.cy/cablenet.com.cy.config.js
+++ sites/cablenet.com.cy/cablenet.com.cy.config.js
@@ -10,69 +10,52 @@ dayjs.extend(customParseFormat);
 
 module.exports = {
   site: 'cablenet.com.cy',
-  days: 6, // maxdays=6
+  days: 6,
   request: {
     cache: {
       ttl: 60 * 60 * 1000 // 1 hour
-    },
-    headers: {
-      'Accept-Encoding': 'gzip, deflate, br'
     }
   },
   url({ date }) {
-    const formattedDate = date.format('YYYY-MM-DD');
-    return `https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg${formattedDate}.json`;
+    return `https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg${date.format('YYYY-MM-DD')}.json`;
   },
-  async parser({ content, channel }) {
-    const shows = [];
-    let data;
+  parser: function ({ content }) {
+    const parsedData = JSON.parse(content);
+    const programs = [];
 
-    try {
-      if (content.trim().length === 0) {
-        throw new Error('Empty response content');
-      }
-      data = JSON.parse(content);
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-      return shows; // Return empty shows array if parsing fails
-    }
-
-    data.forEach(item => {
-      if (item.id === channel.site_id) {
-        item.pr.forEach(pr => {
-          const show = {
-            title: pr.ti || '',
-            startTime: dayjs(pr.df).utc().format(),
-            endTime: dayjs(pr.dt).utc().format(),
-            description: pr.ld || 'No description available',
-            rating: pr.ma || ''
+    Object.keys(parsedData).forEach(id => {
+      const channel = parsedData[id];
+      if (channel.pr) {
+        channel.pr.forEach(item => {
+          const program = {
+            title: item.ti,
+            start: parseDateTime(item.df),
+            stop: parseDateTime(item.dt),
+            description: item.sd || item.ld
           };
-          shows.push(show);
+          programs.push(program);
         });
       }
     });
 
-    return shows;
+    return programs;
   },
   async channels() {
-    const url = `https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg${dayjs().format('YYYY-MM-DD')}.json`;
-    const response = await axios.get(url, {
-      headers: {
-        'Accept-Encoding': 'gzip, deflate, br'
-      }
-    });
+    const data = await axios
+      .get(`https://cablenet.com.cy/wp-content/plugins/tv-guide-plugin/data/epg2024-12-05.json`)
+      .then(r => r.data)
+      .catch(console.error);
 
-    const data = response.data;
-    const channels = [];
+    const channelData = Object.keys(data).map(channelId => ({
+      lang: 'el',
+      name: data[channelId].ch,
+      site_id: data[channelId].id
+    }));
 
-    data.forEach(channel => {
-      channels.push({
-        lang: 'el',
-        name: channel.ch || 'Channel Name Unknown',
-        site_id: channel.id.toString()
-      });
-    });
-
-    return channels;
+    return channelData;
   }
 };
+
+function parseDateTime(dateTime) {
+  return dayjs(dateTime, 'YYYYMMDDHHmmss').format('YYYY-MM-DDTHH:mm:ssZ');
+}
