7d512a80d5ab77207d10ae2e598326434db4e5f2
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-09 01:19:25 -0500

    Update siminn.is.config.js

--- sites/siminn.is/siminn.is.config.js
+++ sites/siminn.is/siminn.is.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   url({ channel, date }) {
     const formattedDate = date.format('YYYY-MM-DD');
-    return `https://api.tv.siminn.is/oreo-api/v2/channels/${channel.site_id}/events?start=${formattedDate}T00:00&end=${formattedDate}T23:59`;
+    return `https://siminn-proxy.siminn.is/api/getChannels?channelId=${channel.site_id}&time=${formattedDate}`;
   },
   async parser({ content }) {
     const shows = [];
@@ -38,41 +38,22 @@ module.exports = {
       return shows; // Return empty shows array if parsing fails
     }
 
-    data.forEach(item => {
-      const show = {
-        title: item.title || '',
-        description: item.description || 'No description available',
-        episode: '',
-        season: '',
-        subtitle: '',
-        icon: '',
-        originalTitle: item.originalTitle || '',        
-        start: dayjs(item.start).utc().format(),
-        stop: dayjs(item.end).utc().format()
-      };
-
-      if (item.episode) {
-        show.subtitle = item.episode.title || '';
-        show.episode = item.episode.episodeNumber ? `E${item.episode.episodeNumber}` : '';
-        if (item.episode.seasonNumber) {
-          show.episode = `S${item.episode.seasonNumber}${show.episode}`;
-        }
-        if (item.episode.episodeCount) {
-          show.episode = `${show.episode}/${item.episode.episodeCount}`;
-        }
-      }
-
-      if (item.images && item.images.length > 0) {
-        show.icon = `https://api.tv.siminn.is${item.images[0].url}`;
-      }
-
-      shows.push(show);
-    });
+    if (data && data.epg) {
+      data.epg.forEach(item => {
+        const show = {
+          title: item.title || '',
+          description: item.description || 'No description available',
+          start: dayjs(item.start).utc().format(),
+          stop: dayjs(item.end).utc().format()
+        };
+        shows.push(show);
+      });
+    }
 
     return shows;
   },
   async channels() {
-    const url = 'https://www.siminn.is/dagskra';
+    const url = 'https://www.siminn.is/sjonvarp';
     const response = await axios.get(url, {
       headers: {
         'Accept-Encoding': 'gzip, deflate, br'
@@ -82,8 +63,8 @@ module.exports = {
     const $ = cheerio.load(response.data);
     const channels = [];
 
-    $('div[data-name="dagsskraitemimg"]').each((index, element) => {
-      const siteId = $(element).find('img').attr('src').match(/\/([^\/]+)\.png/)[1];
+    $('.channelinfo').each((index, element) => {
+      const siteId = $(element).find('.channelID').text().trim();
       const name = $(element).find('.channelName').text().trim();
 
       channels.push({
