770c55851cc1ab07aedbcbe83dde2705b2b6d75c
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-09 10:50:08 -0500

    Update yes.co.il.config.js

--- sites/yes.co.il/yes.co.il.config.js
+++ sites/yes.co.il/yes.co.il.config.js
@@ -4,8 +4,6 @@ const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
 const customParseFormat = require('dayjs/plugin/customParseFormat');
 
-const API_ENDPOINT = 'https://www.yes.co.il/o/yes/servletlinearsched';
-
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
@@ -20,7 +18,7 @@ module.exports = {
   },
   url({ date }) {
     const formattedDate = date.utc().format('YYYYMMDD');
-    return `${API_ENDPOINT}/getscheduale?startdate=${formattedDate}&p_auth=ue2qQse6`;
+    return `https://www.yes.co.il/o/yes/servletlinearsched/getscheduale?startdate=${formattedDate}&p_auth=ue2qQse6`;
   },
   parser({ content }) {
     const programs = JSON.parse(content).schedule || [];
@@ -38,7 +36,7 @@ module.exports = {
     // Modify this part based on how you fetch channels information
     const axios = require('axios');
     try {
-      const response = await axios.get('${API_ENDPOINT}/getchannels?p_auth=ue2qQse6');
+      const response = await axios.get('https://www.yes.co.il/o/yes/servletlinearsched/getchannels?p_auth=ue2qQse6');
       return response.data.map(channel => {
         return {
 		  lang: 'he',
