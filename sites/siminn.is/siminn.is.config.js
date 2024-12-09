1dda690757baa194c408133c611675206f1f79d5
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-09 00:39:02 -0500

    Update siminn.is.config.js

--- sites/siminn.is/siminn.is.config.js
+++ sites/siminn.is/siminn.is.config.js
@@ -3,6 +3,7 @@ const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
 const customParseFormat = require('dayjs/plugin/customParseFormat');
+const cheerio = require('cheerio');
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
